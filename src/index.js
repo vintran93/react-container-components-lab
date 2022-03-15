44fd4ca690112c5f26042481d5949dfd5ac72c81
Vincent Tran <vincentuong@yahoo.com>
2022-03-15 16:26:17 -0500

    commit tree 1df3ae7736f3c15f1c420ab1e5489858a8586d12

--- src/index.js
+++ src/index.js
@@ -9,5 +9,6 @@ ReactDOM.render(
     <SearchableMovieReviewsContainer />
     <LatestMovieReviewsContainer />
   </div>,
-  document.getElementById('root')
+  document.getElementById('global')
 );
+
